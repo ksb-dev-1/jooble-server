fc05ab79700b0fed75781649e52b94249567cc17
ksb-dev-1 <ksbabaleshwar@gmail.com>
2025-07-07 07:05:33 +0530

    add Suspense to JobDetailsPage.tsx

--- src/app/jobs/[job_id]/page.tsx
+++ src/app/jobs/[job_id]/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
 
 // lib
 import { getUserSession } from "@/lib/getUserSession";
@@ -7,6 +8,7 @@ import { getUserSession } from "@/lib/getUserSession";
 // components
 import Container from "@/components/shared/Container";
 import Breadcrumb from "@/components/shared/BreadCrumb";
+import JobDetailsSkeleton from "@/components/skeletons/JobDetailsSkeleton";
 import JobDetails from "@/components/job-details/JobDetails";
 
 export const metadata: Metadata = {
@@ -44,7 +46,9 @@ export default async function JobDetailsPage({
           Job Details
         </h1>
 
-        <JobDetails params={params} userId={userId} />
+        <Suspense fallback={<JobDetailsSkeleton />}>
+          <JobDetails params={params} userId={userId} />
+        </Suspense>
       </main>
     </Container>
   );
